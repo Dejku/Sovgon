95419d9ae8121ac153feca8c8f91dbcf8b9194dc
Dejkuu <dawio246@op.pl>
2024-02-28 17:40:45 +0100

    ://

--- src/Events/ready.js
+++ src/Events/ready.js
@@ -3,8 +3,7 @@ import chalk from 'chalk';
 const name = 'ready';
 const once = true;
 
-async function execute(client) {
-	await client.user.setPresence({ status: 'online', activities: [{ name: '/help' }] });
+function execute() {
 	console.log(`Status: ${chalk.greenBright('READY')}`);
 }
 
